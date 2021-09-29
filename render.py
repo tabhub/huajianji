9b34d96f0d87d8fd31aaabbe68988c0b12e6853e
B1nj0y <idegorepl@gmail.com>
2021-09-29 20:28:22 +0800

    Add TabHub resource manifest

--- render.py
+++ render.py
@@ -41,24 +41,24 @@ def get_image(_id):
         image_map[_id] = image
         return image
 
-dirs = [
-    ('花间集', '赵崇祚'),
-    ('南唐二主词', '李煜 李璟'),
-    ('唐诗三百首', '蘅塘退士'),
-    ('宋词三百首', ''),
-    ('教科书选诗', '教科书出版社\n包含人民教育出版社、江苏教育出版社等'),
-    ('古诗十九首', '无名氏'),
-    ('诗经', '佚名'),
-    ('千家诗', '南宋·謝枋得\n明代·王相'),
-    ('声律启蒙', '清·车万育'),
-    ('三字经', '王應麟'),
-    ('唐诗三百首·蒙学', '清代·蘅塘退士'),
-]
+dirs = {
+        '花间集':  ('赵崇祚', 'huajianji'),
+        '南唐二主词': ('李煜 李璟', 'erzhuci'),
+        '唐诗三百首': ('蘅塘退士', 'tangshi300'),
+        '宋词三百首': ('', 'songci300'),
+        '教科书选诗': ('教科书出版社\n包含人民教育出版社、江苏教育出版社等', 'jks_shixuan'),
+        '古诗十九首': ('无名氏', 'gushi19'),
+        '诗经': ('佚名', 'shijing'),
+        '千家诗': ('南宋·謝枋得\n明代·王相', 'qianjiashi'),
+        '声律启蒙':  ('清·车万育', 'shenglvqimeng'),
+        '三字经': ('王應麟', 'sanzijing'),
+        '唐诗三百首·蒙学': ('清代·蘅塘退士', 'tangshi300mengxue'),
+        }
 
 paths = []
 
-for n, a in dirs:
-    pfiles  = os.listdir(u'./data/%s/' % n)
+for name, info in dirs.items():
+    pfiles = os.listdir(u'./data/%s/' % name)
 
     for pfile in pfiles:
         if 'json' not in pfile:
@@ -67,16 +67,17 @@ for n, a in dirs:
         if 'author' in pfile:
             continue
 
-        path = os.path.join(u'./data/%s/' % n, pfile)
+        path = os.path.join(u'./data/%s/' % name, pfile)
         juan = pfile.split('.')[1]
-        paths.append((n, path, juan))
-        
+        paths.append((name, path, juan))
+
 
 books = defaultdict(list)
+tabhub = {}
 
 for book, path, juan in paths:
 
-    books[(book, dict(dirs)[book])].append(juan)
+    books[(book, dirs[book][0])].append(juan)
 
     with open(path, 'r', encoding="utf-8") as f:
         content = f.read()
@@ -88,7 +89,7 @@ for book, path, juan in paths:
         poetry["id"] = str(hash(juan + poetry["title"])).replace('-','')
 
         note_items = poetry.get('notes', [])
-        
+
         for note in note_items:
             if '--' in note:
                 left, right = note.split('--')
@@ -100,25 +101,33 @@ for book, path, juan in paths:
             first = left[0]
             if first.isdigit():
                 left = left.replace(first, '').replace('.', '')
-        
+
             first = right[0]
             if first.isdigit():
                 right  = right.replace(first, '').replace('.', '')
             notes.append((left, right))
-        
+
         poetry["notes"] = notes
 
         root = '../../'
         image = get_image(poetry["id"])
         template = template_env.get_template(WORD_TEMPLATE_FILE)
         output = template.render(poetry=poetry, juan=juan, image=image, root=root)
-        
+
         html_filename = 'www/poetrys/%s.html' % poetry["id"]
-        
+
         with open(html_filename, 'w', encoding="utf-8") as f:
              f.write(output)
 
-    
+        item = {"name": "%s" % poetry["id"],
+                "type": "tabhub-app",
+                "url": "https://tabhub.io/huajianji/www/poetrys/%s.html" % poetry["id"]}
+        if book in tabhub:
+            tabhub[book].append(item)
+        else:
+            tabhub[book] = [item]
+
+
     root = '../../'
     image = get_image(juan)
     template = template_env.get_template('list.html')
@@ -126,23 +135,30 @@ for book, path, juan in paths:
     with open('www/list/%s.html' % juan, 'w', encoding="utf-8") as f:
         f.write(output)
 
+for book, its in tabhub.items():
+    pinyin = dirs[book][1]
+    r = {"name": book, "type": "resource", "version": "1.0", "items": its}
+    with open('tabhub/%s.json' % pinyin, 'w', encoding="utf-8") as f:
+        f.write(json.dumps(r, indent=2, ensure_ascii=False))
+    print("[%s](https://raw.githubusercontent.com/tabhub/huajianji/master/tabhub/%s.json)" % (book, pinyin))
+
 for book, juans in books.items():
-    root = '../'        
+    root = '../'
     image = get_image(str(hash(book[0])).replace('-', ''))
     template = template_env.get_template('book.html')
     output = template.render(book=book, juans=juans, image=image, root=root)
-            
+
     with open('www/%s.html' % book[0], 'w', encoding="utf-8") as f:
         f.write(output)
-        
+
 root = './'
 image = get_image('index')
 template = template_env.get_template('index.html')
 output = template.render(books=books, image=image, author="", root=root)
 with open('index.html' ,'w', encoding="utf-8") as f:
     f.write(output)
-        
+
 
 with open('.image.json', 'w', encoding="utf-8") as f:
     f.write(json.dumps(image_map, indent=2, ensure_ascii=False))
-    
+
